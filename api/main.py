2bb20f36ee48ce56aab9930471b8418c5e0dc7dd
Sanwal Khan <sialsanwal885@gmail.com>
2023-01-30 21:37:38 -0700

    converted image into numpy array

--- api/main.py
+++ api/main.py
@@ -1,11 +1,23 @@
-from fastapi import FastAPI
+import numpy as np
+from fastapi import FastAPI, UploadFile,File
 import uvicorn
+from io import BytesIO
+from PIL import Image
 
 app= FastAPI()
 
 @app.get("/ping")
 async def ping():
     return "Hello World"
+def read_file_as_image(data) ->np.ndarray:
+    image= np.array(Image.open(BytesIO(data)))
+    return image
+@app.post("/predict")
+async def predict(
+        file: UploadFile = File(...)
 
+):
+    bytes= read_file_as_image(await file.read())
+    pass
 if __name__=="__main__":
     uvicorn.run(app,host='localhost',port=8000)
