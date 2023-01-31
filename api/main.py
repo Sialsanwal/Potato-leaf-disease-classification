503ef0ee2f16873e14471b1aa8e2c042eaf1d47e
Sanwal Khan <sialsanwal885@gmail.com>
2023-01-30 21:50:39 -0700

    update

--- api/main.py
+++ api/main.py
@@ -3,8 +3,14 @@ from fastapi import FastAPI, UploadFile,File
 import uvicorn
 from io import BytesIO
 from PIL import Image
+import tensorflow as tf
+
+
 
 app= FastAPI()
+
+MODEL= tf.keras.models.load_model("../saved_models/1")
+CLASS_NAMES =["Early Blight","Late Blight","Healthy"]
 
 @app.get("/ping")
 async def ping():
@@ -18,6 +24,8 @@ async def predict(
 
 ):
     bytes= read_file_as_image(await file.read())
+    img_batch = np.expand_dims(image, 0)
+    predictions= MODEL.predict(img_batch)
     pass
 if __name__=="__main__":
     uvicorn.run(app,host='localhost',port=8000)
