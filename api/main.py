8c1d64434099f25638fcf9b0d87067d409635da4
Sanwal Khan <sialsanwal885@gmail.com>
2023-02-22 14:39:45 -0700

    update

--- api/main.py
+++ api/main.py
@@ -1,31 +1,36 @@
+
 import numpy as np
 from fastapi import FastAPI, UploadFile,File
 import uvicorn
 from io import BytesIO
 from PIL import Image
 import tensorflow as tf
-
-
+from tensorflow.python.client import device_lib
+device_lib.list_local_devices()
 
 app = FastAPI()
-
-MODEL = tf.keras.models.load_model("../saved_models/1")
-CLASS_NAMES =["Early Blight","Late Blight","Healthy"]
-
 @app.get("/ping")
 async def ping():
-    return "Hello World"
-def read_file_as_image(data) ->np.ndarray:
+    return "Hello I am Alive"
+
+def read_file_as_image(data) -> np.ndarray:
     image = np.array(Image.open(BytesIO(data)))
     return image
+
 @app.post("/predict")
 async def predict(
         file: UploadFile = File(...)
 
 ):
-    bytes = read_file_as_image(await file.read())
-    img_batch = np.expand_dims(image, 0)
-    predictions = MODEL.predict(img_batch)
+    image = read_file_as_image(await file.read())
+
+
     pass
-if __name__=="__main__":
-    uvicorn.run(app,host='localhost',port=8000)
+
+
+
+if __name__== "__main__":
+    uvicorn.run(app, host='localhost',port=8080)
+
+
+
